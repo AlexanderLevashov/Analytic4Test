2a475fe620ecf1e21ec2271fb200b90726f792a2
Левашов А.В <u0250@C0250.fc.chromatec.ru>
2023-04-05 14:31:36 +0300

    *

--- Analytic4Tests/PageObjects/CommonPageObject/PlannerPageObject/AnalysisPageObject.cs
+++ Analytic4Tests/PageObjects/CommonPageObject/PlannerPageObject/AnalysisPageObject.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Analytic4Tests.PageObjects.PageObjectPlanner
+namespace Analytic4Tests.PageObjects.CommonPageObject.PlannerPageObject
 {
     public class AnalysisPageObject
     {
